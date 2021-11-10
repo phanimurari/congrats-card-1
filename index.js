c63e48516a2bb14ffb09cc660e15563f8d6f8eed
ussvarma <saivarmauddaraju@gmail.com>
2021-11-10 12:38:38 +0000

    congrats-card

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = ( 
+<div className="bg-container">
+    <div >
+        <h1 className="heading">Congratulations</h1>
+    <div>
+    <div className="card1">
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"/>
+        <div className="description">
+            <h1>Kiran V</h1>
+            <p>vishnu institute of computer education and technology bhimavaram</p>
+        </div>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+
+</div>);
+
+ReactDOM.render(element, document.getElementById("root"));
